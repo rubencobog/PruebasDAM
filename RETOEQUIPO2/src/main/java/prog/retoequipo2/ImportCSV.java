a4491a576ed572c4426f8293601b5bb7fff69783
liamzz05 <lidiamupa@gmail.com>
2025-05-16 17:36:11 +0200

    Update ImportCSV.java

--- RETOEQUIPO2/src/main/java/prog/retoequipo2/ImportCSV.java
+++ RETOEQUIPO2/src/main/java/prog/retoequipo2/ImportCSV.java
@@ -112,7 +112,7 @@ public class ImportCSV {
     }
 
     private static double calcularDistancia(double lat1, double lon1, double lat2, double lon2) {
-        final int R = 6371; // Radio Tierra en km
+        final int R = 6371; 
         double dLat = Math.toRadians(lat2 - lat1);
         double dLon = Math.toRadians(lon2 - lon1);
         double a = Math.sin(dLat / 2) * Math.sin(dLat / 2)
